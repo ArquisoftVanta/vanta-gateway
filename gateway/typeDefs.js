be95260953d7fddbc65e7b166ec9870b7b5e0230
Mauricio Meza <mmezab@unal.edu.co>
2021-04-11 16:48:17 -0500

    initial typeDefs defined

--- gateway/typeDefs.js
+++ gateway/typeDefs.js
@@ -1,3 +1,31 @@
 export const userTypeDef = `
+	type User = {
+		user_id = Int!
+		name = String!
+		doc = String!
+		phone = String!
+		mail = String!
+		address = String!
+		registerDate = String!
+	}
+	input registerInput = {
+		name = String!
+		doc = String!
+		phone = String!
+		mail = String!
+		address = String!
+	}
+	input loginInput = {
+		mail = String!
+		password = String!
+	}`;
 
-`
+export const userQueries =`
+      userById(user_id: Int!): User!
+  `;
+
+export const userMutations = `
+      registerUser(user: registerInput!): User!
+      loginUser(user: loginInput!): String!
+      updateUser(user: registerInput!): User! 
+  `;
