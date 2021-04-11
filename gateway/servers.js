8a7dfe6e57d186df188fa06c34847b3e06b5abb6
Mauricio Meza <mmezab@unal.edu.co>
2021-04-11 15:37:05 -0500

    first commit of graphql gateway

---
+++ gateway/servers.js
@@ -0,0 +1,26 @@
+export const url = 'localhost';
+export const gateway = '8000';
+
+export const auth = '8100';
+export const authEP = 'auth';
+
+export const perfil = '8200';
+export const perfilEP = 'profile';
+
+export const request = '8300';
+export const requestEP = 'request';
+
+export const viajes = '8400';
+export const viajesEP = 'service';
+
+export const vehicles = '8500';
+export const vehicles = 'vehicles';
+
+export const chat = '8600';
+export const chatEP = 'chat';
+
+export const multimedia = '8700';
+export const multimediaEP = 'mult';
+
+export const notificaciones = '8800';
+export const notificacionesEP = 'nots';
